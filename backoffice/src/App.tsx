99c50b0e633feca01b35f93fd564624a47f10409
Isac Araujo <isac_lp@yahoo.com.br>
2020-01-30 13:31:54 -0300

    Create the basis of Superb Backoffice (react app)

---
+++ backoffice/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="main">
+      <header>
+        <p>Superb Backoffice</p>
+      </header>
+      <main>
+        <p>Main content</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
